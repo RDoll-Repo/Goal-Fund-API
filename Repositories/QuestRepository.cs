624ddd987e2dbf0162affda943ae81f671f611c9
Roland Doll <83877587+RDoll-Repo@users.noreply.github.com>
2023-10-28 23:33:58 -0400

    getall

--- Repositories/QuestRepository.cs
+++ Repositories/QuestRepository.cs
@@ -16,33 +16,6 @@ public class QuestRepository : IQuestRepository
     {
         _efCoreService = efCoreService;
     }
-    private static readonly List<Quest> _examples = new()
-    {
-        new Quest
-        {
-            Id = Guid.Parse("3f384a19-45be-4661-8ae5-eab45f04920b"),
-            TaskName = "Do the dishes",
-            Completed = false,
-            Reward = 1,
-            Frequency = Frequency.ONCE
-        },
-        new Quest
-        {
-            Id = Guid.Parse("ff6d61e0-44cf-4640-8681-d4d28186c14e"),
-            TaskName = "Study Swift",
-            Completed = false,
-            Reward = 1,
-            Frequency = Frequency.DAILY
-        },
-        new Quest
-        {
-            Id = Guid.Parse("b4da5e97-f5d1-405c-8c7e-ccfc5a20a21a"),
-            TaskName = "Do taxes",
-            Completed = true,
-            Reward = 3,
-            Frequency = Frequency.YEARLY
-        }
-    };
 
     public async Task<Quest> CreateQuest(Quest quest)
     {
@@ -60,15 +33,17 @@ public class QuestRepository : IQuestRepository
 
     public async Task<SearchResults<Quest>> GetAllQuests()
     {
-        // TODO: This will be asynchronous once we implement the database
-        var results = new SearchResults<Quest>
+        var results = await _efCoreService.WhereAsync(t => true);
+
+        var response = new SearchResults<Quest>
         {
-            ReturnedResults = _examples.Count(),
-            TotalResults = _examples.Count() * 10,  // TODO: Just a placeholder. 
-            Results = _examples
+            ReturnedResults = results.Count(),
+            // TODO: placeholder, actual pagination
+            TotalResults = results.Count() * 10,
+            Results = results.ToList()
         };
 
-        return results;
+        return response;
     }
 
 }
